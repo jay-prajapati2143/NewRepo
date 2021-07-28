5efe9bac8295a3a091085eb39b55b8aad2b12332
jay-prajapati2143 <jayprajapti7085@gmail.com>
2021-07-28 11:08:06 +0530

    File addition

---
+++ AngularJS/namesController.js
@@ -0,0 +1,8 @@
+angular.module('testApp',[]).controller('nameCtrl', function($scope){
+    $scope.names= [
+        {name : "Jay", country : 'USA'},
+        {name : "Tirth", country : 'India'},
+        {name : "Dharmik", country : 'Englend'},
+
+    ]
+})
