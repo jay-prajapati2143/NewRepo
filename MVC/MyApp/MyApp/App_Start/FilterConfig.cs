79b70c3993e9e91988bdd924f3605815f91a005c
jay-prajapati2143 <78398871+jay-prajapati2143@users.noreply.github.com>
2024-02-22 14:23:17 +0530

    Master branch commit

--- MVC/MyApp/MyApp/App_Start/FilterConfig.cs
+++ MVC/MyApp/MyApp/App_Start/FilterConfig.cs
@@ -8,6 +8,15 @@ namespace MyApp
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            
+            //dfhd dfhdf df;dhfa
+            //    dfdhfa
+            //    fdfhd;falsedfhd;flhad
+            //    fhdfl;dhfdf
+            //    adfhadfjhd;lfadfa
+            //    d
+            //    dfhadhfad;lfa
+            //    dfadhladh
         }
     }
 }
